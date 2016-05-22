02a6bb4ea5d01479a10597b4e1aad6fdf5ce886c
lethargilistic <mikeoverby@outlook.com>
2016-05-21 22:34:48 -0700

    Mocked all basic pieces, filled out movement types

--- movement.py
+++ movement.py
@@ -1,28 +1,32 @@
 class MoveRule():
     NORMAL = 0
     STRIKE = 1
-    SLIDE = 2
-    JUMPSLIDE = 3 
+    JUMP = 2
+    SLIDE = 3
+    JUMPSLIDE = 4 
+    COMMAND = 5
+    DREAD = 6
+    DEFENSE = 7
 
     @staticmethod
     def string(enum):
-        return ["NORMAL", "STRIKE", "SLIDE", "JUMPSLIDE"][enum]
+        return ["NORMAL", "STRIKE", "JUMP", "SLIDE", "JUMPSLIDE", "COMMAND", "DREAD", "DEFENSE"][enum]
 
 class Move():
     def __init__(self, x, y, rule=MoveRule.NORMAL):
-        #TODO: Guarantee types. num, num, bool*3
+        #TODO: Guarantee types. num, num, MoveType
         self.x = x
         self.y = y
         self.rule = rule 
 
-    def get_x():
-        return x
+    def get_x() -> int:
+        return self.x
 
     def get_y():
-        return y
+        return self.y
 
     def get_rule():
-        return rule
+        return self.rule
 
     def __str__(self):
         return "("+str(self.x)+","+str(self.y)+","+MoveRule.string(self.rule)+")"
--- pieces.py
+++ pieces.py
@@ -37,31 +37,217 @@ class Piece():
     def get_player(self):
         return self.player
 
-    def get_id(self):
+    def get_id(self) -> int:
         return self.get_id
 
     def __str__(self):
         return self.get_name()+str(self.get_player())
 
-class Footman(Piece):
+class Assassin(Piece):
     def __init__(self, x, y, player):
-        Piece.__init__(self, "F", x, y, player)
+        Piece.__init__(self, "A", x, y, player)
 
     def move1(self):
-        return (Move(0,1), Move(0,-1), Move(1,0), Move(-1,0))
+        return (Move(0,2,MoveRule.JUMPSLIDE),
+                Move(2,-2,MoveRule.JUMPSLIDE), 
+                Move(-2,-2,MoveRule.JUMPSLIDE)) 
 
     def move2(self):
-        return (Move(1,1), Move(1,-1), Move(-1,1), Move(-1,-1),
-                Move(0,2))
+        return (Move(0,-2,MoveRule.JUMPSLIDE),
+                Move(2,2,MoveRule.JUMPSLIDE), 
+                Move(-2,2,MoveRule.JUMPSLIDE)) 
+
+class Bowman(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "S", x, y, player)
+
+    def move1(self):
+        return (Move(0,1), Move(1,0), Move(-1,0),
+                Move(2,0,MoveRule.JUMP), Move(0,-2,MoveRule.JUMP),
+                Move(-2,0,MoveRule.Jump))
+
+    def move2(self):
+        return (Move(0,1), Move(1,-1), Move(-1,-1),
+                Move(0,2,MoveRule.JUMP),Move(1,1,MoveRule.JUMP),
+                Move(-1,1,MoveRule.JUMP))
+
+
+class Champion(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "C", x, y, player)
+
+    def move1(self):
+        return (Move(2,0,MoveRule.JUMP), Move(1,0,MoveRule.NORMAL),
+                Move(0,2,MoveRule.JUMP), Move(0,1,MoveRule.NORMAL), 
+                Move(-2,0,MoveRule.JUMP), Move(-1,0,MoveRule.NORMAL), 
+                Move(0,-2,MoveRule.JUMP), Move(0,-1,MoveRule.NORMAL))
+
+    def move2(self):
+        return (Move(2,0,MoveRule.JUMP), Move(1,0,MoveRule.STRIKE),
+                Move(0,2,MoveRule.JUMP), Move(0,1,MoveRule.STRIKE), 
+                Move(-2,0,MoveRule.JUMP), Move(-1,0,MoveRule.STRIKE), 
+                Move(0,-2,MoveRule.JUMP), Move(0,-1,MoveRule.STRIKE))
+
+class Dragoon(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "Ḑ", x, y, player)
+
+    def move1(self):
+        return (Move(1,0), Move(-1,0),
+                Move(0,2,MoveRule.STRIKE), Move(2,2,MoveRule.STRIKE),
+                Move(-2,2,MoveRule.STRIKE))
+
+    def move2(self):
+        return (Move(0,1), Move(0,2), 
+                Move(1,2,MoveRule.JUMP), Move(-1,2,MoveRule.JUMP),
+                Move(1,-1,MoveRule.SLIDE), Move(-1,-1,MoveRule.SLIDE))
+
 
 class Duke(Piece):
     def __init__(self, x, y, player):
         Piece.__init__(self, "D", x, y, player)
 
     def move1(self):
-            return (Move(1,0,MoveRule.SLIDE), Move(-1,0,MoveRule.SLIDE))
+        return (Move(1,0,MoveRule.SLIDE), Move(-1,0,MoveRule.SLIDE))
 
     def move2(self):
-            return (Move(0,1,MoveRule.SLIDE), Move(0,-1,MoveRule.SLIDE))
+        return (Move(0,1,MoveRule.SLIDE), Move(0,-1,MoveRule.SLIDE))
 
+class Duchess(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "U", x, y, player)
 
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+class Footman(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "F", x, y, player)
+
+    def move1(self):
+        return (Move(0,1), Move(0,-1), 
+                Move(1,0), Move(-1,0))
+
+    def move2(self):
+        return (Move(1,1), Move(1,-1), 
+                Move(-1,1), Move(-1,-1),
+                Move(0,2))
+
+class General(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "G", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+class Knight(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "K", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+class Longbowman(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "K", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+class Marshall(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "M", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+class Oracle(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "O", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+class Pikeman(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "P", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+class Priest(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "T", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+class Ranger(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "R", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+class Seer(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "S", x, y, player)
+
+    def move1(self):
+        return (Move(1,1), Move(1,-1), Move(-1,-1), Move(-1,1),
+                Move(0,2,MoveRule.JUMP), Move(2,0,MoveRule.JUMP),
+                Move(0,-2,MoveRule.JUMP), Move(-2,0,MoveRule.JUMP))
+
+    def move2(self):
+        return (Move(1,0), Move(0,-1), Move(-1,0), Move(0,1),
+                Move(2,2,MoveRule.JUMP), Move(2,-2,MoveRule.JUMP),
+                Move(-2,-2,MoveRule.JUMP), Move(-2,2,MoveRule.JUMP))
+
+class Wizard(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "W", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+
+'''
+class Champion(Piece):
+    def __init__(self, x, y, player):
+        Piece.__init__(self, "C", x, y, player)
+
+    def move1(self):
+        raise NotImplementedError()
+
+    def move2(self):
+        raise NotImplementedError()
+'''
